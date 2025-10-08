11ba8d88207af8a677c40e9f1d7b790164307d54
sknikusha <skoroveronika1605@gmail.com>
2025-10-08 12:40:16 +0400

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
- double nalog(double hour, double stavka, double procent){
-   double nal=hour*stavka+hour*stavka*(procent*0.01);
-   return nal*0.13 ;
- }
+double nalog(double hour, double stavka, double procent){
+  double nal=hour*stavka+hour*stavka*(procent*0.01);
+  return nal*0.13 ;
+}
 
+double sum(double hour, double stavka, double procent){
+  double zarplata=hour*stavka+hour*stavka*(procent*0.01);
+  return zarplata;
+}
 
- double suma(double hour, double stavka, double procent){
-   double nal=nalog(hour,stavka,procent);
-   return (hour*stavka+hour*stavka*(procent*0.01))-nal;
- }
+double finish(double hour, double stavka, double procent){
+  double nal=nalog(hour,stavka,procent);
+  double zar_sum=sum(hour,stavka,procent);
+  return zar_sum-nal;
+}
 
  int main(){
     double hour,stavka,procent;
@@ -23,8 +28,7 @@ using namespace std;
     cin >> procent;
     cout << "общая заработанная ставка S= " << hour*stavka+hour*stavka*(procent*0.01) << endl;
     cout <<"налог = " << nalog(hour, stavka, procent) << endl;
-    double zarplata=suma(hour, stavka, procent);
-    cout <<"сумма на руки " << zarplata << endl;
+    cout <<"сумма на руки " << finish(hour, stavka, procent) << endl;
     return 0;
 
  }
