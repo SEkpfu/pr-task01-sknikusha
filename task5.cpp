1b616271db9eb8a0dba584a2f02fd1c82008bc8d
sknikusha <skoroveronika1605@gmail.com>
2025-10-08 12:42:48 +0400

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 using namespace std;
 
-void infa(int& hour,int& stavka){
+void infa(double& hour,double& stavka, double& procent){
     cout << "кол-во отработанных часов"<<  endl;
     cin >> hour;
     
     cout << "ставка по часовой оплаты"<< endl;
     cin >> stavka;
 
+    cout << "%премии"<< endl;
+    cin >> procent;
+
+
+}
+
+void summa(double& hour,double& stavka, double& procent, double& zar_sum, double& nalog){
+
+    zar_sum=hour*stavka+hour*stavka*(procent*0.01);
+    nalog=(hour*stavka+hour*stavka*(procent/100))*0.13;
+    double pol_sum=(hour*stavka+hour*stavka*(procent/100))-nalog;
 
 }
 
 int main() {
-    int hour1, stavka1, sum1;
-    int hour2, stavka2, sum2;
+    double hour1, stavka1,procent1, zar_sum1,nalog1;
+    double hour2, stavka2,procent2, zar_sum2,nalog2;
 
     cout << "данные 1раб"<<  endl;
-    infa(hour1,stavka1);
+    infa(hour1,stavka1,procent1);
+    summa(hour1,stavka1,procent1,zar_sum1,nalog1);
+
 
     cout << "данные 2раб"<<  endl;
-    infa(hour2,stavka2);
-
-    sum1=hour1*stavka1;
-    sum2=hour2*stavka2;
+    infa(hour2,stavka2,procent2);
+    summa(hour2,stavka2,procent2,zar_sum2,nalog2);
     
-    if(sum1>sum2){
+    if((zar_sum1-nalog1)>(zar_sum2-nalog2)){
         cout<<"1rab"<< endl;
     }
     else{
         cout<<"2rab"<< endl;
     }
 
-    cout << "сумма заработанная двумя s= " << sum1+sum2 << endl;
+    cout << "сумма заработанная двумя s= " << (zar_sum1-nalog1)+(zar_sum2-nalog2) << endl;
     return 0;
 }
+
