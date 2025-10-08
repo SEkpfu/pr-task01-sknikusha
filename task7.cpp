7eda686ce8bfd8c4d2c9efc140149dc937230dc5
sknikusha <skoroveronika1605@gmail.com>
2025-10-08 12:44:02 +0400

    Update task7.cpp

--- task7.cpp
+++ task7.cpp
@@ -1,59 +1,46 @@
 #include<iostream>
 using namespace std;
 
-int hour1, stavka1;
-int hour2, stavka2;
-int hour3, stavka3;
-int hour4, stavka4;
-int hour5, stavka5;
+double hour, stavka,procent;
 
-int sum(int hour, int stavka){
-    int zarplata=hour*stavka;
+double nalog(double hour, double stavka, double procent){
+    double nal=hour*stavka+hour*stavka*(procent*0.01);
+    return nal*0.13 ;
+  }
+
+double sum(double hour, double stavka, double procent){
+    double zarplata=hour*stavka+hour*stavka*(procent*0.01);
     return zarplata;
 }
 
-void infa(int& hour,int& stavka){
+double finish(double hour, double stavka, double procent){
+    double nal=nalog(hour,stavka,procent);
+    double zar_sum=sum(hour,stavka,procent);
+    return zar_sum-nal;
+}
+
+void infa(double& hour,double& stavka, double& procent){
     cout << "кол-во отработанных часов"<<  endl;
     cin >> hour;
     
     cout << "ставка по часовой оплаты"<< endl;
     cin >> stavka;
 
+    cout << "%премии"<< endl;
+    cin >> procent;
 }
 
 int main(){
-    
-    cout <<"введите данные 1раб"<< endl;
-    infa(hour1,stavka1);
-    int s1=sum(hour1,stavka1);
-    
-    cout <<"введите данные 2раб"<< endl;
-    infa(hour2,stavka2);
-    int s2=sum(hour2,stavka2);
+    double summ=0;
+    for(int i=1;i<6;++i){
+        cout <<"введите данные раб номер " << i << endl;
+        infa(hour,stavka,procent);
+        summ+=finish(hour,stavka,procent);
+    }
 
-    
-    cout <<"введите данные 3раб"<< endl;
-    infa(hour3,stavka3);
-    int s3=sum(hour3,stavka3);
 
-    
-    cout <<"введите данные 4раб"<< endl;
-    infa(hour4,stavka4);
-    int s4=sum(hour4,stavka4);
+    cout << "общая заработанная сумма = " << summ << endl;
 
-    
-    cout <<"введите данные 5раб"<< endl;
-    infa(hour5,stavka5);
-    int s5=sum(hour5,stavka5);
-
-    double sr_zarplata=(s1+s2+s3+s4+s5)/5;
-
-    cout << "общая заработанная сумма 1раб = " << s1 << endl;
-    cout << "общая заработанная сумма 2раб = " << s2 << endl;
-    cout << "общая заработанная сумма 3раб = " << s3 << endl;
-    cout << "общая заработанная сумма 4раб = " << s4 << endl;
-    cout << "общая заработанная сумма 5раб = " << s5 << endl;
-
-    cout << "средняя зп в бригаде = " << sr_zarplata << endl;
+    cout << "средняя зп в бригаде = " << summ/5.0 << endl;
     return 0;
 }
